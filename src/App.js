9b9d5c68f211b6d864de6de857aa7ac09f2e2fe8
starkxj <starkxj@163.com>
2019-02-28 16:31:22 +0800

    添加 todo

--- src/App.js
+++ src/App.js
@@ -10,18 +10,32 @@ class App extends Component {
     super(props)
     this.state = {
       newTodo: '',
-      todoList: [
-        {id:1, title:'待办1'},
-        {id:2, title:'待办2'},
-        {id:3, title:'待办3'}
-      ]
+      todoList: []
     }
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
   }
   render(){
     let todos = this.state.todoList.map((elem, index) => {
       //return <li>{todo.title}</li>
       return (
-        <li key={elem.id}>
+        <li key={index}>
           <TodoItem todo={elem}/>
         </li>
       )
@@ -30,7 +44,9 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="InputWrapper">
-          <TodoInput content={this.state.newTodo}/>
+          <TodoInput content={this.state.newTodo}
+            onSubmit={this.addTodo.bind(this)}
+            onChange={this.changeTitle.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -41,3 +57,11 @@ class App extends Component {
 }
 
 export default App;
+
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
 
 export default class TodoInput extends Component{
+  submit(e){
+    if(e.key === 'Enter'){
+      this.props.onSubmit(e)
+    }
+  }
+  changeTitle(e){
+    this.props.onChange(e)
+  }
   render(){
     return(
-      <input type="text" value={this.props.content}/>
+      <input type="text" value={this.props.content}
+        onKeyPress={this.submit.bind(this)}
+        onChange={this.changeTitle.bind(this)}/>
     )
   }
 }
