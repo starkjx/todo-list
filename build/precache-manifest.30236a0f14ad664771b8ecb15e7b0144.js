f3b3abd0f0845a92b0e6ec6b8a569501a3b366e2
starkxj <starkxj@163.com>
2019-02-28 11:05:15 +0800

    react init

---
+++ build/precache-manifest.30236a0f14ad664771b8ecb15e7b0144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3860a4526c40baadfa65",
+    "url": "/static/js/main.3860a452.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3860a4526c40baadfa65",
+    "url": "/static/css/main.2368de70.chunk.css"
+  },
+  {
+    "revision": "99e80d083c815c99103a3df38ed7e6c5",
+    "url": "/index.html"
+  }
+];
